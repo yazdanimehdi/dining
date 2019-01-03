f0ae84d3dbfbe92657ca1b922b570c70750c6410
Mehdi <my_jahromi@yahoo.com>
2019-01-04 01:45:09 +0330

    خاموش کردن سوختن زوروکد برای بتا تست

--- dining/views/change_info_dining.py
+++ dining/views/change_info_dining.py
@@ -5,16 +5,18 @@ from dining.models import UserDiningData, Coins
 
 def change_info_dining(request):
     if request.user.is_authenticated:
-        u = UserDiningData.objects.filter(user=request.user)
+        c = UserDiningData.objects.filter(user=request.user)
         a = Coins.objects.filter(user=request.user, active=True).count()
         if request.method == 'GET':
-            if u:
+            if c:
+                u = UserDiningData.objects.get(user=request.user)
                 return render(request, 'dining/templates/change_info_dining .html',
                               {'username': u.dining_username,
                                'password': u.dining_password})
             else:
                 return redirect('/wizard')
         elif request.method == 'POST':
+            u = UserDiningData.objects.get(user=request.user)
             try:
                 u.dining_username = request.POST.get('dining_username')
                 u.dining_password = request.POST.get('dining_password')
