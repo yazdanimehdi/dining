f25c78dbdeff958b311fab75ff53f4f820998e64
Mehdi <my_jahromi@yahoo.com>
2019-01-05 10:26:55 +0330

    test index

--- dining/views/changeinfo_days.py
+++ dining/views/changeinfo_days.py
@@ -13,35 +13,35 @@ def change_days(request):
                 return redirect('/wizard')
         elif request.method == 'POST':
             a = Coins.objects.filter(user=request.user, active=True).count()
+            u = UserDiningData.objects.get(user=request.user)
+            u.reserve_saturday_lunch = False
+            u.reserve_sunday_lunch = False
+            u.reserve_monday_lunch = False
+            u.reserve_tuesday_lunch = False
+            u.reserve_wednesday_lunch = False
+            u.reserve_thursday_lunch = False
+            u.reserve_friday_lunch = False
+
+            u.reserve_saturday_breakfast = False
+            u.reserve_sunday_breakfast = False
+            u.reserve_monday_breakfast = False
+            u.reserve_tuesday_breakfast = False
+            u.reserve_wednesday_breakfast = False
+            u.reserve_thursday_breakfast = False
+            u.reserve_friday_breakfast = False
+
+            u.reserve_saturday_dinner = False
+            u.reserve_sunday_dinner = False
+            u.reserve_monday_dinner = False
+            u.reserve_tuesday_dinner = False
+            u.reserve_wednesday_dinner = False
+            u.reserve_thursday_dinner = False
+            u.reserve_friday_dinner = False
 
             try:
-                u = UserDiningData.objects.get(user=request.user)
+
                 for x in request.POST:
                     if hasattr(u, x):
-                        u.reserve_saturday_lunch = False
-                        u.reserve_sunday_lunch = False
-                        u.reserve_monday_lunch = False
-                        u.reserve_tuesday_lunch = False
-                        u.reserve_wednesday_lunch = False
-                        u.reserve_thursday_lunch = False
-                        u.reserve_friday_lunch = False
-
-                        u.reserve_saturday_breakfast = False
-                        u.reserve_sunday_breakfast = False
-                        u.reserve_monday_breakfast = False
-                        u.reserve_tuesday_breakfast = False
-                        u.reserve_wednesday_breakfast = False
-                        u.reserve_thursday_breakfast = False
-                        u.reserve_friday_breakfast = False
-
-                        u.reserve_saturday_dinner = False
-                        u.reserve_sunday_dinner = False
-                        u.reserve_monday_dinner = False
-                        u.reserve_tuesday_dinner = False
-                        u.reserve_wednesday_dinner = False
-                        u.reserve_thursday_dinner = False
-                        u.reserve_friday_dinner = False
-
                         u.__setattr__(x, request.POST.get(x))
                 u.save()
             except:
