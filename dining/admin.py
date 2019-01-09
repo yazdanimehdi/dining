faead65516ee54d8541f460399756ca3689ba97f
Mehdi <my_jahromi@yahoo.com>
2019-01-09 16:15:13 +0330

    اضافه کردن زمان عضویت

--- dining/admin.py
+++ dining/admin.py
@@ -8,7 +8,7 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['username', 'phone', 'sex', 'is_paid']
+    list_display = ['username', 'phone', 'sex', 'is_paid', 'Date_joined']
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
