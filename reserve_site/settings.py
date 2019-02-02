08953152fbe0995cd4cd0c04ca5e3b7d993b1182
Mehdi <my_jahromi@yahoo.com>
2019-02-02 22:16:12 +0330

    تغییر سازوکار درگاه پرداخت

--- reserve_site/settings.py
+++ reserve_site/settings.py
@@ -126,10 +126,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'diningdb',
-        # 'USER': 'admin',
-        # 'PASSWORD': '99&3M+p`gw5{v%Jv',
-        'USER': 'postgres',
-        'PASSWORD': 'salam1392',
+        'USER': 'admin',
+        'PASSWORD': '99&3M+p`gw5{v%Jv',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'salam1392',
         'HOST': 'localhost',
         'port': '5432'
     }
