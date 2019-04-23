a41b9db53ed4c10f4c514b8ef181a407d3935989
Mehdi <my_jahromi@yahoo.com>
2019-04-24 00:10:13 +0430

    تصحیح مکان استاتیک ها

--- reserve_site/settings.py
+++ reserve_site/settings.py
@@ -185,7 +185,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/yazdani/dining/static/'
+STATIC_ROOT = '/home/yazdani/dining/static/'
 AUTH_USER_MODEL = 'dining.CustomUser'
 LOGIN_REDIRECT_URL = "/dashboard"
 AUTH_PROFILE_MODULE = 'dining.dashboard'
